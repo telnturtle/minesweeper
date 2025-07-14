3582187f3e685e24582a6f022b2948d3effddb53
엄휘용 <telnturtle@gmail.com>
2025-07-14 16:00:34 +0900

    ESLint 설정, Emotion 플러그인, TypeScript 세팅, Vite base path 업데이트, 안쓰는 CSS, 에셋 삭제

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,13 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
   },
 ])
